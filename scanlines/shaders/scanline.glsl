ca00fd30ec7ae04dafbe6ad05718dacf48f33e21
Jonatan <32527868+rotmoss@users.noreply.github.com>
2021-02-12 22:32:01 +0100

    Add files via upload

--- scanlines/shaders/scanline.glsl
+++ scanlines/shaders/scanline.glsl
@@ -1,10 +1,3 @@
-// Parameter lines go here:
-#pragma parameter SCANLINE_BASE_BRIGHTNESS "Scanline Base Brightness" 0.95 0.0 1.0 0.01
-#pragma parameter SCANLINE_SINE_COMP_A "Scanline Sine Comp A" 0.0 0.0 0.10 0.01
-#pragma parameter SCANLINE_SINE_COMP_B "Scanline Sine Comp B" 0.15 0.0 1.0 0.05
-
-#define pi 3.141592654
-
 #if defined(VERTEX)
 
 #if __VERSION__ >= 130
@@ -28,9 +21,7 @@ COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
 COMPAT_VARYING vec4 COL0;
 COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING vec2 omega;
 
-vec4 _oPosition1; 
 uniform mat4 MVPMatrix;
 uniform COMPAT_PRECISION int FrameDirection;
 uniform COMPAT_PRECISION int FrameCount;
@@ -40,10 +31,8 @@ uniform COMPAT_PRECISION vec2 InputSize;
 
 void main()
 {
-    gl_Position = MVPMatrix * VertexCoord;
-    COL0 = COLOR;
+    gl_Position = VertexCoord.x * MVPMatrix[0] + VertexCoord.y * MVPMatrix[1] + VertexCoord.z * MVPMatrix[2] + VertexCoord.w * MVPMatrix[3];
     TEX0.xy = TexCoord.xy;
-	omega = vec2(pi * OutputSize.x, 2.0 * pi * TextureSize.y);
 }
 
 #elif defined(FRAGMENT)
@@ -76,31 +65,14 @@ uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING vec2 omega;
-
-// compatibility #defines
-#define Source Texture
-#define vTexCoord TEX0.xy
-
-#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
-#define OutputSize vec4(OutputSize, 1.0 / OutputSize)
-
-#ifdef PARAMETER_UNIFORM
-// All parameter floats need to have COMPAT_PRECISION in front of them
-uniform COMPAT_PRECISION float SCANLINE_BASE_BRIGHTNESS;
-uniform COMPAT_PRECISION float SCANLINE_SINE_COMP_A;
-uniform COMPAT_PRECISION float SCANLINE_SINE_COMP_B;
-#else
-#define SCANLINE_BASE_BRIGHTNESS 0.95
-#define SCANLINE_SINE_COMP_A 0.0
-#define SCANLINE_SINE_COMP_B 0.15
-#endif
 
 void main()
 {
-   vec2 sine_comp = vec2(SCANLINE_SINE_COMP_A, SCANLINE_SINE_COMP_B);
-   vec3 res = COMPAT_TEXTURE(Source, vTexCoord).xyz;
-   vec3 scanline = res * (SCANLINE_BASE_BRIGHTNESS + dot(sine_comp * sin(vTexCoord * omega), vec2(1.0, 1.0)));
-   FragColor = vec4(scanline.x, scanline.y, scanline.z, 1.0);
+    if (mod(floor(TEX0.y * OutputSize.y), 2) == 0) {
+    	FragColor = COMPAT_TEXTURE(Texture, TEX0.xy);
+    }
+    else {
+    	FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
 } 
 #endif
